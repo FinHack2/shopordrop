6585ce49e5ba520c984d81b57093432abe6010ae
franceneee <57038559+franceneee@users.noreply.github.com>
2022-12-03 23:36:57 +0800

    Update classes with new fields

--- ShopOrDropApp/ShopOrDropApp/Models/PurchaseItem.cs
+++ ShopOrDropApp/ShopOrDropApp/Models/PurchaseItem.cs
@@ -4,6 +4,7 @@
     {
         public string ID { get; set; }
         public string UserID { get; set; }
+        public string ItemName { get; set; }
         public string Category { get; set; }
         public float ItemCost { get; set; }
         public string DayOfWeek { get; set; }
