4731afd2fbe46d5df0f2421d1ccc2d163234fb3d
franceneee <57038559+franceneee@users.noreply.github.com>
2022-12-04 11:20:16 +0800

    Add login page

---
+++ ShopOrDropApp/ShopOrDropApp/Models/LoginPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using ShopOrDropApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOrDropApp.Views.ViewModels
+{
+    public partial class LoginPageViewModel : BaseViewModel
+    {
+        [ObservableProperty]
+        private string email;
+        [ObservableProperty]
+        private string password;
+
+        [RelayCommand]
+        public void Login()
+        {
+
+        }
+    }
+}
